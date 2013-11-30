f0dec8fc90ee91177f171594bddd1b3f89cad097
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-11-29 23:40:01 -0800

    removed blog bits

--- my_app.rb
+++ my_app.rb
@@ -5,13 +5,6 @@ require 'yaml'
 
 class MyApp < Sinatra::Base
 
-  before do 
-    @posts = Dir.glob("views/posts/*.erb").map do |post_name|
-      post_name.split("/").last.slice(0..-5)
-    end
-    @sorted_posts = meta_data.sort_by {|post, date_hash| date_hash["date"] }.reverse
-  end
-
   get "/" do
     erb :index, :locals => {:title => "Totally Legit Horoscopes"}
   end
@@ -30,26 +23,5 @@ class MyApp < Sinatra::Base
 
   get "/faq" do
     erb :faq, :locals => {:title => "FAQ"}
-  end
-
-  get "/blog/:post_name" do
-    html = erb("posts/#{params[:post_name]}".to_sym, layout: false,)
-    html = html.split("\n\n", 2).last
-    erb html, :locals => {:title => "BLOG!"}
-    # erb "/posts/#{params[:post_name]}".to_sym, 
-  end
-
-  def meta_data
-    if @meta_data
-      @meta_data
-    else
-      @meta_data = {}
-      @posts.each do |post|
-        html = erb("/posts/#{post}".to_sym, layout: false)
-        meta = YAML.load(html.split("\n\n", 2).first)
-        @meta_data[post] = meta
-      end
-      @meta_data
-    end
   end
 end
